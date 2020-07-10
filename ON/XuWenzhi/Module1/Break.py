283e6d721aff49c6baff88fd44d41a1111378b17
Wenzhi <xuwenzhi7@gmail.com>
2020-07-10 12:35:32 -0700

    For Module1 :smile:

---
+++ ON/XuWenzhi/Module1/Break.py
@@ -0,0 +1,15 @@
+print("For-Break")
+for letter in 'Python':
+    if letter == 'h':
+        break
+    print('Current Leter :', letter)
+
+print("\nWhile-Break")
+var = 10
+while var > 0:
+    print('Current variable value : ', var)
+    var = var - 1
+    if var == 5:
+        break
+    
+print('Good Bye!')
---
+++ ON/XuWenzhi/Module1/Numbers.py
@@ -0,0 +1,8 @@
+print("\nFloat\n")
+a = 2.2
+b = 2
+c = 0.1
+print ("a + b = ", a + b)
+print ("a + c = ", a + c)
+print ("a * b = ", a * b)
+print ("a ** b = ", a ** b)
