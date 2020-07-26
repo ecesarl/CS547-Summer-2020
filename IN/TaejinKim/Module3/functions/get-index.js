203bcd2c50502fc2f606779ad4cdc2e59880a4d9
aviatortj <ktj81m@hotmail.com>
2020-07-26 01:46:44 -0700

    Submission for Module3

---
+++ IN/TaejinKim/Module3/functions/get-index.js
@@ -0,0 +1,10 @@
+
+'use strict';
+
+module.exports.handler = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    body: 'My first API.',
+  };
+  callback(null, response);
+};
