b31851df2ae2ae590316088d5b218e404a0bad34
brianrgeving <brianrgeving@gmail.com>
2020-08-09 12:40:27 -0700

    Submission for Module 5

--- ON/BrianGeving/Module2/bookstore/functions/get-index.js
+++ ON/BrianGeving/Module2/bookstore/functions/get-index.js
@@ -1,7 +1,9 @@
 'use strict';
 
 const fs = require('fs'); // Require the fs library to read files
-
+// Template library.
+const Mustache = require('mustache'); // Template library.
+const axios = require('axios');
 var html; // Set a variable outside of function in order to reuse
 
 // The function that will read content from our html file
@@ -17,16 +19,22 @@ const getHtml = () => {
   });
 };
 
+const fetchBooks = () => axios.get(process.env.fetch_books_api);
 // The main Lambda function
 module.exports.handler = async (event, context, callback) => {
   const htmlcontent = await getHtml(); // Get the content
+  const books = await fetchBooks();
 
+  const returnHtml = Mustache.render(htmlcontent, {
+    books: books.data.Items,
+  });
   const response = {
     statusCode: 200,
-    headers: { // Set up the header
+    // Set up the header
+    headers: {
       'Content-Type': 'text/html; charset=UTF-8',
     },
-    body: htmlcontent,
+    body: returnHtml,
   };
   callback(null, response);
 };
