bb79174cd2017fc3e2b84227f707af9a47f8bf7a
YUCHE-Liu <watch776@yahoo.com.rw>
2020-08-18 01:09:43 -0700

    Submission for Moudle6

--- IN/bookstore/functions/get-index.js
+++ IN/bookstore/functions/get-index.js
@@ -2,6 +2,12 @@
 
 const fs = require('fs'); // Require the fs library to read files
 
+const Mustache = require('mustache'); // Template library.
+const axios = require('axios');
+const aws4 = require('aws4');
+const awscred = require('awscred');
+const URL = require('url');
+
 var html; // Set a variable outside of function in order to reuse
 
 // The function that will read content from our html file
@@ -14,6 +20,39 @@ const getHtml = () => {
       html = data;
       resolve(html);
     });
+  });
+};
+
+const fetchBooks = async () => {
+  // Use AWS4 to sign the request
+  const url = URL.parse(process.env.fetch_books_api);
+  const opts = {
+    host: url.hostname,
+    path: url.pathname,
+  };
+  if (!process.env.AWS_ACCESS_KEY_ID || !process.env.AWS_SECRET_ACCESS_KEY) await new Promise((resolve, reject) => {
+    awscred.loadCredentials((err, data) => {
+      if (err) reject(err);
+      process.env.AWS_ACCESS_KEY_ID = data.accessKeyId;
+      process.env.AWS_SECRET_ACCESS_KEY = data.secretAccessKey;
+      if (data.sessionToken) process.env.AWS_SESSION_TOKEN = data.sessionToken;
+      resolve();
+    });
+  });
+
+  aws4.sign(opts);
+
+  const headers = {
+    Host: opts.headers.Host,
+    'X-Amz-Date': opts.headers['X-Amz-Date'],
+    Authorization: opts.headers.Authorization,
+    'X-Amz-Security-Token': opts.headers['X-Amz-Security-Token'],
+  };
+  // If 'X-Amz-Security-Token' does not exsit, delete it for the local test.
+  if (!headers['X-Amz-Security-Token']) delete headers['X-Amz-Security-Token'];
+
+  return axios.get(process.env.fetch_books_api, {
+    headers,
   });
 };
 
